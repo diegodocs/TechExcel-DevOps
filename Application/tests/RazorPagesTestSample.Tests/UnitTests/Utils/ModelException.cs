161385543009fa9519158691a92da477d50df68a
Diego Cardoso <DiegoCardoso@microsoft.com>
2023-12-06 17:13:18 -0300

    add tests for message limit size

---
+++ Application/tests/RazorPagesTestSample.Tests/UnitTests/Utils/ModelException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace RazorPagesTestSample.Tests.UnitTests.Utils
+{
+    [Serializable]
+    public class ModelException : Exception
+    {
+        public ModelException(string message, IEnumerable<ValidationResult> errors) : base(message)
+        {
+            Errors = errors;
+        }
+
+        public ModelException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected ModelException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public IEnumerable<ValidationResult> Errors { get; protected set; }
+    }
+}
